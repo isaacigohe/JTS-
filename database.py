917e408cd89fdcb3db2425f259ac8a87e7115543
isaacigohe <isaacigohe120@gmail.com>
2026-05-12 10:53:07 +0300

    import the use of bcrypt in email password

--- database.py
+++ database.py
@@ -15,5 +15,31 @@ class Database:
         
         # Create indexes
         self.setup_indexes()
-
-
+        
+        
+    def setup_indexes(self):
+        """Create necessary indexes for optimization"""
+        # Unique email index
+        self.users.create_index("email", unique=True)
+        # Job indexes for searching
+        self.jobs.create_index("job_title")
+        self.jobs.create_index("company")
+        self.jobs.create_index("location")
+        # Compound index for saved_jobs
+        self.saved_jobs.create_index([("user_id", 1), ("job_id", 1)], unique=True)
+    
+    def register_user(self, email, password):
+        """Register a new user with hashed password"""
+        try:
+            # Hash password
+            hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+            
+            user_doc = {
+                "email": email,
+                "password": hashed_password,
+                "registration_date": datetime.now(),
+                "last_login": None
+            }
+            
+            result = self.users.insert_one(user_doc)
+            return True, str(result.inserted_id)
